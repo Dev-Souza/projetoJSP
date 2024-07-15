8c37feeff37e0e8c2925350e601aeb2cd1251029
Dev-Souza <kauan.souza.dev@gmail.com>
2024-07-15 14:02:56 -0300

    Página de login

--- src/main/java/servlets/ServletLogin.java
+++ src/main/java/servlets/ServletLogin.java
@@ -2,6 +2,7 @@ package servlets;
 
 import java.io.IOException;
 
+import dao.DAOLoginRepository;
 import jakarta.servlet.RequestDispatcher;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
@@ -11,54 +12,65 @@ import jakarta.servlet.http.HttpServletResponse;
 import model.ModelLogin;
 
 /*O chamando Controller são as servlets ou ServletLoginController*/
-@WebServlet(urlPatterns = {"/principal/ServletLoginn", "/ServletLoginn"}) /*Mapeamento de URL que vem da tela*/
+@WebServlet(urlPatterns = { "/principal/ServletLoginn", "/ServletLoginn" }) /* Mapeamento de URL que vem da tela */
 public class ServletLogin extends HttpServlet {
-	
+
 	private static final long serialVersionUID = 1L;
 
-    public ServletLogin() {
-    }
+	private DAOLoginRepository daoLoginRepository = new DAOLoginRepository();
 
-    /*Recebe os dados pela url em parametros*/
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	public ServletLogin() {
+	}
+
+	/* Recebe os dados pela url em parametros */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 
 	}
 
-	/*recebe os dados enviados por um formulario*/
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		
+	/* recebe os dados enviados por um formulario */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+
 		String login = request.getParameter("login");
 		String senha = request.getParameter("senha");
 		String url = request.getParameter("url");
-		
-		if (login != null && !login.isEmpty() && senha != null && !senha.isEmpty()) {
-			
-			ModelLogin modelLogin = new ModelLogin();
-			modelLogin.setLogin(login);
-			modelLogin.setSenha(senha);
-			
-			if (modelLogin.getLogin().equalsIgnoreCase("admin")
-					&& modelLogin.getSenha().equalsIgnoreCase("admin")) { /*Simulando login*/
-				
-				request.getSession().setAttribute("usuario", modelLogin.getLogin());
-				
-				if (url == null || url.equals("null")) {
-					url = "principal/principal.jsp";
+
+		try {
+
+			if (login != null && !login.isEmpty() && senha != null && !senha.isEmpty()) {
+
+				ModelLogin modelLogin = new ModelLogin();
+				modelLogin.setLogin(login);
+				modelLogin.setSenha(senha);
+
+				if (daoLoginRepository.validarAutenticacao(modelLogin)) { /* Simulando login */
+
+					request.getSession().setAttribute("usuario", modelLogin.getLogin());
+
+					if (url == null || url.equals("null")) {
+						url = "principal/principal.jsp";
+					}
+
+					response.sendRedirect(url);
+
+				} else {
+					request.setAttribute("msg", "Informe o login e senha corretamente!");
+					RequestDispatcher redirecionar = request.getRequestDispatcher("/index.jsp");
+					redirecionar.forward(request, response);
 				}
-				
-				response.sendRedirect(url);
-				
+
 			} else {
 				request.setAttribute("msg", "Informe o login e senha corretamente!");
 				RequestDispatcher redirecionar = request.getRequestDispatcher("/index.jsp");
 				redirecionar.forward(request, response);
 			}
-			
-		} else {
-			request.setAttribute("msg", "Informe o login e senha corretamente!");
-			RequestDispatcher redirecionar = request.getRequestDispatcher("/index.jsp");
+		} catch (Exception e) {
+			e.printStackTrace();
+			RequestDispatcher redirecionar = request.getRequestDispatcher("erro.jsp");
+			request.setAttribute("msg", e.getMessage());
 			redirecionar.forward(request, response);
 		}
-		
+
 	}
 }
--- src/main/webapp/index.jsp
+++ src/main/webapp/index.jsp
@@ -1,33 +1,97 @@
 <%@ page language="java" contentType="text/html; charset=UTF-8"
 	pageEncoding="UTF-8"%>
 <!DOCTYPE html>
-<html>
+<html lang="en">
 <head>
 <meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link
+	href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css"
+	rel="stylesheet"
+	integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH"
+	crossorigin="anonymous">
 <title>Curso JSP</title>
+
+<style type="text/css">
+	form {
+		position: absolute;
+		top: 40%;
+		left: 33%;
+		right: 33%;
+	}
+	
+	h5 {
+		position: absolute;
+		top: 30%;
+		left: 33%;
+		right: 33%;
+	}
+	
+	h6 {
+		position: absolute;
+		top: 65%;
+		left: 33%;
+		right: 33%;
+		font-size: 15px;
+		color: red;
+	}
+</style>
 </head>
 <body>
 
-	<h1>Bem vindo ao curso de JSP</h1>
+	<h5>Bem vindo ao curso de JSP</h5>
 
-	<form action="ServletLoginn" method="post">
-	<input type="hidden" value="<%= request.getAttribute("url") %>" name="url">
-		<table>
-			<tr>
-				<td><label>Login: </label></td>
-				<td><input name="login" type="text"></td>
-			</tr>
-			<tr>
-				<td><label>Senha: </label></td>
-				<td><input name="senha" type="password"></td>
-			</tr>
-			<tr>
-				<td></td>
-				<td><input type="submit" value="Enviar"></td>
-			</tr>
-		</table>
+	<form action="ServletLoginn" method="post" class="row g-3 needs-validation" novalidate>
+		<input type="hidden" value="<%=request.getParameter("url")%>"
+			name="url">
+		<div class="col-md-6">
+			<label class="form-label" for="login">Login: </label> 
+			<input name="login" id="login" type="text" class="form-control" required="required">
+			<div class="valid-feedback">
+      			OK
+    		</div>
+    		<div class="invalid-feedback">
+      			Informe o login.
+    		</div>
+		</div>
+
+		<div class="col-md-6">
+			<label class="form-label" for="senha">Senha: </label> 
+			<input name="senha" type="password" class="form-control" id="senha" required="required">
+			<div class="valid-feedback">
+      			OK
+    		</div>
+    		<div class="invalid-feedback">
+      			Informe a senha.
+    		</div>
+		</div>
+		
+		<input class="btn btn-primary" type="submit" value="Enviar">
+
 	</form>
 
-	<h4>${msg}</h4>
+	<h6>${msg}</h6>
+
+	<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+	<script type="text/javascript">
+		(() => {
+		  'use strict'
+
+		  // Fetch all the forms we want to apply custom Bootstrap validation styles to
+		  const forms = document.querySelectorAll('.needs-validation')
+
+		  // Loop over them and prevent submission
+		  Array.from(forms).forEach(form => {
+		    form.addEventListener('submit', event => {
+		      if (!form.checkValidity()) {
+		        event.preventDefault()
+		        event.stopPropagation()
+		      }
+
+		      form.classList.add('was-validated')
+		    }, false)
+		  })
+		})()	
+	</script>
 </body>
 </html>
